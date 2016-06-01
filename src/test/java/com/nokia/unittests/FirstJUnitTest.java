73b89010eb6474266a72fbe534cf74294c3fea06
Hejnowicz <rafal.hejnowicz@alcatel-lucent.com>
2016-06-01 14:36:46 +0200

    committed

---
+++ src/test/java/com/nokia/unittests/FirstJUnitTest.java
@@ -0,0 +1,32 @@
+package com.nokia.unittests;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstJUnitTest {
+	
+	@Test
+	public void should(){
+		System.out.println("Hello world");
+		Assert.assertThat("hello", IsEqual.equalTo("world"));
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("start");
+//
+		System.out.println("in progress...");
+//
+		System.out.println("stop");
+	}
+}
+
+
+// given
+// when
+// then
+
+//@Before
+//@After
---
+++ src/test/java/com/nokia/unittests/WyszukiwanieElementuPoIndeksie.java
@@ -0,0 +1,46 @@
+package com.nokia.unittests;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WyszukiwanieElementuPoIndeksie {
+
+	//@Before
+	//
+	
+	@Test
+	public void wyszukZwracaEl0(){
+	// given
+	// nieprzypisana wartosc tab[0]
+		//int arr[] = int[3];
+
+	//when
+		arr[0] = 0;
+		int i = 0;
+		
+	//then
+		Assert.assertThat(arr[0], IsEqual.equalTo(i));
+		//System.out.println(arr[0]);
+	}
+
+	@Test
+	public void wyszukZwracaEl1() {
+		// given
+		String arr[] = new String[3];
+
+		arr[0] = "el1";
+
+		// when
+		String e = "el1";
+
+		// then
+		Assert.assertThat(arr[0], IsEqual.equalTo(e));
+	}
+
+}
+
+// git add
+// git commit (akceptacja zmian lokalnie)
+// git pull (pobranie z serwera)
+// git push origin master:rafalh

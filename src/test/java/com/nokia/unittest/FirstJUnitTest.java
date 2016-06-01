e54fa023dfdb2287496506f40ce20619cea8906c
U-EMEA\mpawlows <mpawlows@PLMPAWLOWS-C1.emea.lucent.com>
2016-06-01 14:27:33 +0200

    initial commit

---
+++ src/test/java/com/nokia/unittest/FirstJUnitTest.java
@@ -0,0 +1,37 @@
+package com.nokia.unittest;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.*;
+
+public class FirstJUnitTest {
+
+	static List<String> TestList = new ArrayList<>();
+	int listSize;
+	
+	@BeforeClass
+	public static void AddList() {
+		TestList.add("item_1");
+		TestList.add("item_2");
+		TestList.add("item_3");
+		TestList.add("item_4");
+	}
+	
+	@Test
+	public void shouldElementAddedToList() {
+		TestList.add("item_5");
+		Assert.assertEquals("item_5", TestList.get(TestList.size()-1));
+	}
+
+	@Test
+	public void shouldElementDeleteFromList() {
+		TestList.remove(TestList.get(TestList.size()-1));
+		Assert.assertEquals("item_5", TestList.get(TestList.size()-1));
+	}
+	
+	@Test
+	public void shouldElementSearchByIndex() {
+		
+	}
+}

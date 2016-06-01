249fefbe3c068ebc3cd6a6bd6060a48cc4a3dcea
Pryjomski <adam.pryjomski@on.nokia.com>
2016-06-01 15:50:51 +0200

    second update

--- src/test/java/com/nokia/unittests/FirstJUnitFromMarcinTest.java
+++ src/test/java/com/nokia/unittests/FirstJUnitFromMarcinTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.hamcrest.core.IsEqual;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -24,11 +25,12 @@ public class FirstJUnitFromMarcinTest {
        public void setUP() {
               System.out.println("Prepare");
               values = new ArrayList<String>();
+              lista = new ArrayList<String>();
        }
        
        @Test
        public void shouldAddElements() {
-    	   System.out.println("Run test 1");
+    	   System.out.println("Run test: #1");
     	   // when
            values.add("element");
            Assert.assertThat(values.get(0), IsEqual.equalTo("element"));
@@ -36,25 +38,30 @@ public class FirstJUnitFromMarcinTest {
        
        @Test
        public void shouldRemoveElements() {
-              System.out.println("Run test 2");
+              System.out.println("Run test: #2");
               values.add("element");
               values.remove(0);
               Assert.assertEquals(values.size(),0);
        }
        
-    		@Test
-    		public void whatIsElementIndex() {
-    			lista.add("pierwszy");
-    			lista.add("drugi");
-    			lista.add("trzeci");
-    			Assert.assertThat(lista.get(1), IsEqual.equalTo("drugi"));
-    			System.out.println("After the test of element indexes: " + lista.get(1));
-    		} 
+    	@Test
+    	public void whatIsElementIndex() {
+    		System.out.println("Run test: #3");
+    		lista.add("pierwszy");
+    		lista.add("drugi");
+    		lista.add("trzeci");
+    		Assert.assertThat(lista.get(1), IsEqual.equalTo("drugi"));
+    		System.out.println("After the test of element indexes: " + lista.get(1));
+    	} 
 
-       
        @After
        public void finishTest() {
               System.out.println("After test");
        }
+       
+       @AfterClass
+       public static void setUpAfterClass() {
+              System.out.println("Finish class");
+       }       
 
 }

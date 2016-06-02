f5c0390f33afb1a899b2efe6cfdb226e8fa3cbb7
Anna <anna.wojtowicz@alcatel-lucent.com>
2016-06-02 10:21:36 +0200

    phones test update3

--- src/main/java/com/nokia/unittests/PhoneValidatorTest.java
+++ src/main/java/com/nokia/unittests/PhoneValidatorTest.java
@@ -3,6 +3,12 @@ package com.nokia.unittests;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class PhoneValidatorTest {
 
 	PhoneValidator phoneValidator;
@@ -20,6 +26,18 @@ public class PhoneValidatorTest {
 	@Test
 	public void checkThatNumber1234567890IsOK() {
 		Assert.assertTrue(phoneValidator.validatePhoneNumber("1234567890"));
+	}
+
+	@Test
+	@Parameters({ "01234", "", "abcdefg", "---", ";.jk" })
+	public void checkThatNumberIsWrong(String value) {
+		Assert.assertFalse(phoneValidator.validatePhoneNumber(value));
+	}
+
+	@Test
+	@Parameters({ "1234567890", "(123)-456-7890", "123 456 7890", "123.456.7890", "123-456-7890 x1234" })
+	public void checkThatNumberIsOK(String value) {
+		Assert.assertTrue(phoneValidator.validatePhoneNumber(value));
 	}
 
 }

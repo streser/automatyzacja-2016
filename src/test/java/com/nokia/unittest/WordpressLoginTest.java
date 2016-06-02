3c7f36ba99bccc051efbe3ae4e5a33bfcff7189c
lothalev <lothalev@gmail.com>
2016-06-02 14:26:54 +0200

    refactor to separate class

--- src/test/java/com/nokia/unittest/WordpressLoginTest.java
+++ src/test/java/com/nokia/unittest/WordpressLoginTest.java
@@ -1,30 +1,11 @@
 package com.nokia.unittest;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
-import java.util.concurrent.TimeUnit;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class WordpressLoginTest {
-
-    private WebDriver driver;
-    private String baseUrl;
-    private StringBuffer verificationErrors = new StringBuffer();
-
-    @Before
-    public void setUp() throws Exception {
-	driver = new FirefoxDriver();
-	baseUrl = "https://wordpress.com";
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
+public class WordpressLoginTest extends SeleniumBase {
 
     @Test
     public void testLoginWordPress() throws Exception {
@@ -35,63 +16,9 @@ public class WordpressLoginTest {
 	waitForElement(By.className("click-wpcom-login"));
     }
 
-    private void wordpressLogout() throws InterruptedException {
-	driver.findElement(By.cssSelector("svg.gridicon.gridicons-user-circle  > g > path")).click();
-	waitForElement(By.xpath("(//button[@type='submit'])[2]"));
-	driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
-    }
-
     private void assertLoginSuccessfull() throws InterruptedException {
 	waitForElement(By.cssSelector("svg.gridicon.gridicons-user-circle"));
 	assertTrue(isElementPresent(By.cssSelector("svg.gridicon.gridicons-user-circle")));
-    }
-
-    private void wordpressLogin(String login, String pass) {
-	openLoginPage();
-	insertTextInto("user_login", login);
-	insertTextInto("user_pass", pass);
-	driver.findElement(By.id("wp-submit")).click();
-    }
-
-    private void openLoginPage() {
-	driver.get(baseUrl + "/");
-	driver.findElement(By.className("click-wpcom-login")).click();
-    }
-
-    private void waitForElement(By selector) throws InterruptedException {
-	for (int second = 0;; second++) {
-	    if (second >= 60)
-		fail("timeout");
-	    try {
-		if (isElementPresent(selector))
-		    break;
-	    } catch (Exception e) {
-	    }
-	    Thread.sleep(1000);
-	}
-    }
-
-    private void insertTextInto(String fieldName, String text) {
-	driver.findElement(By.id(fieldName)).clear();
-	driver.findElement(By.id(fieldName)).sendKeys(text);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-	driver.quit();
-	String verificationErrorString = verificationErrors.toString();
-	if (!"".equals(verificationErrorString)) {
-	    fail(verificationErrorString);
-	}
-    }
-
-    private boolean isElementPresent(By by) {
-	try {
-	    driver.findElement(by);
-	    return true;
-	} catch (NoSuchElementException e) {
-	    return false;
-	}
     }
 
 }

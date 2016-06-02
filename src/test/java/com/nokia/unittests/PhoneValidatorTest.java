100c8d73d938019e7c61f2d746a55f3c4c659e05
Goska <pawel.goska@alcatel-lucent.com>
2016-06-02 10:07:31 +0200

    param tests

--- src/test/java/com/nokia/unittests/PhoneValidatorTest.java
+++ src/test/java/com/nokia/unittests/PhoneValidatorTest.java
@@ -1,10 +1,16 @@
 package com.nokia.unittests;
 
 import org.junit.Assert;
-
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class PhoneValidatorTest {
 	
 	private PhoneValidator phoneValidator;
@@ -16,18 +22,27 @@ public class PhoneValidatorTest {
 	}
 	
 	@Test
-	public void testValidNumber()
+	@Parameters({"1234567890", 
+     			  "123-456-7890",
+     			  "123-456-7890 x1234",
+     			  "123-456-7890",
+     			  "(123)-456-7890"})
+	public void testValidNumber(String phoneNumber)
 	{		
-		Assert.assertTrue(phoneValidator.validatePhoneNumber("1234567890"));
-		Assert.assertTrue(phoneValidator.validatePhoneNumber("123-456-7890"));
-		Assert.assertTrue(phoneValidator.validatePhoneNumber("123-456-7890 x1234"));
+		Assert.assertTrue(phoneValidator.validatePhoneNumber(phoneNumber));
+		
 	}
 	
 	@Test
-	public void testInvalidNumber()
+	@Parameters({"1234567", 
+		  "123-456--7890",
+		  "123-456-x7890 x1234",
+		  "123-4-7890",
+		  "(123-456-7890"})
+	public void testInvalidNumber(String phoneNumber)
 	{
-		Assert.assertFalse(phoneValidator.validatePhoneNumber("123456789a"));
-		Assert.assertFalse(phoneValidator.validatePhoneNumber("123-456-7890 xa1234"));
+		Assert.assertFalse(phoneValidator.validatePhoneNumber(phoneNumber));
+		
 	}
 
 }

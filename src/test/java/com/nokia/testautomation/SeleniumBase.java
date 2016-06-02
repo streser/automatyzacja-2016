c3aa9743573f1e0ef76f4854f5af06f7bdcb45bf
Kranz <karol.kranz@on.nokia.com>
2016-06-02 13:50:12 +0100

    Refactoring superclass

---
+++ src/test/java/com/nokia/testautomation/SeleniumBase.java
@@ -0,0 +1,85 @@
+package com.nokia.testautomation;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public abstract class SeleniumBase {
+
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	public SeleniumBase() {
+		super();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "https://pl.wordpress.com";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+
+	public void logOut() throws InterruptedException {
+		driver.findElement(By.cssSelector("img.gravatar")).click();
+		By logoutButton = By.xpath("(//button[@type='submit'])[2]");
+	    waitForElement();
+	    driver.findElement(logoutButton).click();
+	}
+
+	public void waitForElement() throws InterruptedException {
+		for (int second = 0;; second++) {
+	    	if (second >= 60) fail("timeout");
+	    	try { if (isElementPresent(By.xpath("(//button[@type='submit'])[2]"))) break; } catch (Exception e) {}
+	    	Thread.sleep(1000);
+	    }
+	}
+
+	public void logIn(String login, String password) {
+
+		click(By.className("click-wpcom-login"));
+	    insertText(By.id("user_login"), login);
+	    insertText(By.id("user_pass"), password);
+	    click(By.id("wp-submit"));
+	}
+
+	public void click(By id) {
+		driver.findElement(id).click();
+	}
+
+	public void insertText(By id, String text) {
+		driver.findElement(id).clear();
+	    driver.findElement(id).sendKeys(text);
+	}
+
+	public void open(String path) {
+		driver.get(baseUrl + path);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	protected boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+}
--- src/test/java/com/nokia/testautomation/SeleniumIDETest.java
+++ src/test/java/com/nokia/testautomation/SeleniumIDETest.java
@@ -1,78 +1,20 @@
 package com.nokia.testautomation;
 
-import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.Assert.assertTrue;
 
-public class SeleniumIDETest {
-  private WebDriver driver;
-  private String baseUrl;
-  private StringBuffer verificationErrors = new StringBuffer();
+import org.junit.Test;
+import org.openqa.selenium.By;
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://pl.wordpress.com";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+public class SeleniumIDETest extends SeleniumBase {
+	@Test
+	public void userWithCorrectCredentialsIsSucessfullyLoggedIn() throws Exception {
+		open("/");
+		logIn("szkolenieautomatyzacja", "qw12qw12");
+		assertThatLogInWasSuccesful();
+		logOut();
+	}
 
-  @Test
-  public void testSelen() throws Exception {
-	openLogInForm();
-    logIn("szkolenieautomatyzacja", "qw12qw12");
-    assertThatLogInWasSuccesful();
-    logOut();
-  }
-
-public void assertThatLogInWasSuccesful() {
-	assertTrue(isElementPresent(By.xpath("//header[@id='header']/a[2]/span")));
-}
-
-public void logOut() throws InterruptedException {
-	driver.findElement(By.cssSelector("img.gravatar")).click();
-	By logoutButton = By.xpath("(//button[@type='submit'])[2]");
-    waitForElement();
-    driver.findElement(logoutButton).click();
-}
-
-public void waitForElement() throws InterruptedException {
-	for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if (isElementPresent(By.xpath("(//button[@type='submit'])[2]"))) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
-    }
-}
-
-public void logIn(String login, String password) {
-	
-    driver.findElement(By.id("user_login")).clear();
-    driver.findElement(By.id("user_login")).sendKeys(login);
-    driver.findElement(By.id("user_pass")).clear();
-    driver.findElement(By.id("user_pass")).sendKeys(password);
-    driver.findElement(By.id("wp-submit")).click();
-}
-public void openLogInForm(){
-	driver.get(baseUrl + "/");
-	driver.findElement(By.className("click-wpcom-login")).click();
-}
-
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	public void assertThatLogInWasSuccesful() {
+		assertTrue(isElementPresent(By.xpath("//header[@id='header']/a[2]/span")));
+	}
 }

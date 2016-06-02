9f8b94b67bccbdec112bd105cb423be2faebe2b1
Pryjomski <adam.pryjomski@on.nokia.com>
2016-06-02 14:53:39 +0200

    further refactoring of SeleniumBase

--- src/test/java/com/nokia/unittests/SeleniumBase.java
+++ src/test/java/com/nokia/unittests/SeleniumBase.java
@@ -11,10 +11,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SeleniumBase {
+public abstract class SeleniumBase {
 
+	private static final By LOGOUT_BUTTON_LOCATOR = By.xpath("(//button[@type='submit'])[2]");
 	protected WebDriver driver;
 	private String baseUrl;
+	private String path;
 	private StringBuffer verificationErrors = new StringBuffer();
 
 	public SeleniumBase() {
@@ -25,47 +27,45 @@ public class SeleniumBase {
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
 		baseUrl = "https://pl.wordpress.com";
+		path = "/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	protected void openLoginForm() {
-		driver.get(baseUrl + "/");
+		open(baseUrl + path);
 		driver.findElement(By.linkText("Zaloguj się")).click();
 	}
 
-	protected void logOut() throws InterruptedException {
-		for (int second = 0;; second++) {
-			if (second >= 60)
-				fail("timeout");
-			try {
-				if (isElementPresent(By.cssSelector("img.gravatar")))
-					break;
-			} catch (Exception e) {
-			}
-			Thread.sleep(1000);
-		}
-	
+	private void open(String path) {
+		driver.get(path);
+	}
+
+	protected void logOut() throws InterruptedException {	
+		waitForElement(By.cssSelector("img.gravatar"));
+
 		driver.findElement(By.cssSelector("img.gravatar")).click();
+		waitForElement(LOGOUT_BUTTON_LOCATOR);
+
+		driver.findElement(LOGOUT_BUTTON_LOCATOR).click();
+	}
+
+	private void waitForElement(By locator) throws InterruptedException {
 		for (int second = 0;; second++) {
 			if (second >= 60)
 				fail("timeout");
 			try {
-				if (isElementPresent(By.xpath("(//button[@type='submit'])[2]")))
+				if (isElementPresent(locator))
 					break;
 			} catch (Exception e) {
 			}
 			Thread.sleep(1000);
 		}
-	
-		driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
 	}
 
 	protected void logIn(String login, String password) throws InterruptedException {
-		driver.findElement(By.id("user_login")).clear();
-		driver.findElement(By.id("user_login")).sendKeys(login);
-		driver.findElement(By.id("user_pass")).clear();
-		driver.findElement(By.id("user_pass")).sendKeys(password);
-		driver.findElement(By.id("wp-submit")).click();
+		insertText(By.id("user_login"), login);
+		insertText(By.id("user_pass"), login);
+		click(By.id("wp-submit"));
 		for (int second = 0;; second++) {
 			if (second >= 60)
 				fail("timeout");
@@ -76,6 +76,15 @@ public class SeleniumBase {
 			}
 			Thread.sleep(1000);
 		}
+	}
+
+	private void click(By identifier) {
+		driver.findElement(identifier).click();
+	}
+
+	private void insertText(By identifier, String text) {
+		driver.findElement(identifier).clear();
+		driver.findElement(identifier).sendKeys(text);
 	}
 
 	@After

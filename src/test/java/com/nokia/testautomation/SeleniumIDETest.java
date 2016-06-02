ae16b7add96201cc85231400f09ff65ba26fe835
Kranz <karol.kranz@on.nokia.com>
2016-06-02 13:16:38 +0100

    refactoring methods

--- src/test/java/com/nokia/testautomation/SeleniumIDETest.java
+++ src/test/java/com/nokia/testautomation/SeleniumIDETest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class SeleniumIDETest {
   private WebDriver driver;
   private String baseUrl;
-  private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
   @Before
@@ -21,33 +20,42 @@ public class SeleniumIDETest {
 
   @Test
   public void testSelen() throws Exception {
+	openLogInForm();
     logIn("szkolenieautomatyzacja", "qw12qw12");
+    assertThatLogInWasSuccesful();
     logOut();
   }
+
+public void assertThatLogInWasSuccesful() {
+	assertTrue(isElementPresent(By.xpath("//header[@id='header']/a[2]/span")));
+}
 
 public void logOut() throws InterruptedException {
 	driver.findElement(By.cssSelector("img.gravatar")).click();
 	By logoutButton = By.xpath("(//button[@type='submit'])[2]");
-    waitForElement(logoutButton);
+    waitForElement();
     driver.findElement(logoutButton).click();
 }
 
-public void waitForElement(By byElem) throws InterruptedException {
+public void waitForElement() throws InterruptedException {
 	for (int second = 0;; second++) {
     	if (second >= 60) fail("timeout");
-    	try { if (isElementPresent(byElem)) break; } catch (Exception e) {}
+    	try { if (isElementPresent(By.xpath("(//button[@type='submit'])[2]"))) break; } catch (Exception e) {}
     	Thread.sleep(1000);
     }
 }
 
 public void logIn(String login, String password) {
-	driver.get(baseUrl + "/");
-	driver.findElement(By.className("click-wpcom-login")).click();
+	
     driver.findElement(By.id("user_login")).clear();
     driver.findElement(By.id("user_login")).sendKeys(login);
     driver.findElement(By.id("user_pass")).clear();
     driver.findElement(By.id("user_pass")).sendKeys(password);
     driver.findElement(By.id("wp-submit")).click();
+}
+public void openLogInForm(){
+	driver.get(baseUrl + "/");
+	driver.findElement(By.className("click-wpcom-login")).click();
 }
 
   @After

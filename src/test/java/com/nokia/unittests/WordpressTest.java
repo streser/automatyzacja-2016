6b457c4c992be7e7f377f0ca80a8ecfac24a72b8
Anna <anna.wojtowicz@alcatel-lucent.com>
2016-06-02 14:19:42 +0200

    WordpressTest version1

---
+++ src/test/java/com/nokia/unittests/WordpressTest.java
@@ -0,0 +1,107 @@
+package com.nokia.unittests;
+
+import static org.junit.Assert.*;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WordpressTest {
+
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "https://pl.wordpress.com";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testWordpress() throws Exception {
+		openLogInForm();
+
+		logIn("szkolenieautomatyzacja", "qw12qw12");
+
+		assertThatLogInWasSuccessful();
+		logOut();
+	}
+
+	private void openLogInForm() {
+		driver.get(baseUrl + "/");
+		driver.findElement(By.linkText("Zaloguj się")).click();
+	}
+
+	private void assertThatLogInWasSuccessful() {
+		assertTrue(isElementPresent(By.linkText("Get recommendations on who to follow")));
+	}
+
+	private void logOut() throws InterruptedException {
+		driver.findElement(By.cssSelector("img.gravatar")).click();
+
+		for (int second = 0;; second++) {
+			if (second >= 60)
+				fail("timeout");
+			try {
+				if (isElementPresent(By.xpath("(//button[@type='submit'])[2]")))
+					break;
+			} catch (Exception e) {
+			}
+			Thread.sleep(1000);
+		}
+		driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+	}
+
+	private void logIn(String login, String password) {
+		driver.findElement(By.id("user_login")).clear();
+		driver.findElement(By.id("user_login")).sendKeys(login);
+		driver.findElement(By.id("user_pass")).clear();
+		driver.findElement(By.id("user_pass")).sendKeys(password);
+		driver.findElement(By.id("wp-submit")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+}

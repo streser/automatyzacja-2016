45ddafd7bc5583df53ae5b73fef50c3fcb261518
Pryjomski <adam.pryjomski@on.nokia.com>
2016-06-02 14:20:21 +0200

    refactoring of selenium java test case

--- src/test/java/com/nokia/unittests/FirstSeleniumTest.java
+++ src/test/java/com/nokia/unittests/FirstSeleniumTest.java
@@ -9,96 +9,99 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSeleniumTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://pl.wordpress.com";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "https://pl.wordpress.com";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @Test
-  public void testCaseSeleniumJunitWebdriverJava() throws Exception {
-    driver.get(baseUrl + "/");
-    driver.findElement(By.linkText("Zaloguj się")).click();
-    driver.findElement(By.id("user_login")).clear();
-    driver.findElement(By.id("user_login")).sendKeys("szkolenieautomatyzacja");
-    driver.findElement(By.id("user_pass")).clear();
-    driver.findElement(By.id("user_pass")).sendKeys("qw12qw12");
-    driver.findElement(By.id("wp-submit")).click();
-    for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if (isElementPresent(By.cssSelector("h2.empty-content__title"))) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
-    }
+	@Test
+	public void checkElementName() throws Exception {
+		openLoginForm();
+		logIn("szkolenieautomatyzacja", "qw12qw12");
+		assertThatLoginWasSuccessful();
+		logOut();
+	}
 
-    assertEquals("Welcome to the Reader", driver.findElement(By.cssSelector("h2.empty-content__title")).getText());
-    for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if (isElementPresent(By.cssSelector("img.gravatar"))) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
-    }
+	private void openLoginForm() {
+		driver.get(baseUrl + "/");
+		driver.findElement(By.linkText("Zaloguj się")).click();
+	}
 
-    driver.findElement(By.cssSelector("img.gravatar")).click();
-    for (int second = 0;; second++) {
-    	if (second >= 60) fail("timeout");
-    	try { if (isElementPresent(By.xpath("(//button[@type='submit'])[2]"))) break; } catch (Exception e) {}
-    	Thread.sleep(1000);
-    }
+	private void assertThatLoginWasSuccessful() {
+		assertEquals("Welcome to the Reader", driver.findElement(By.cssSelector("h2.empty-content__title")).getText());
+	}
 
-    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
-  }
+	private void logOut() throws InterruptedException {
+		for (int second = 0;; second++) {
+			if (second >= 60)
+				fail("timeout");
+			try {
+				if (isElementPresent(By.cssSelector("img.gravatar")))
+					break;
+			} catch (Exception e) {
+			}
+			Thread.sleep(1000);
+		}
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+		driver.findElement(By.cssSelector("img.gravatar")).click();
+		for (int second = 0;; second++) {
+			if (second >= 60)
+				fail("timeout");
+			try {
+				if (isElementPresent(By.xpath("(//button[@type='submit'])[2]")))
+					break;
+			} catch (Exception e) {
+			}
+			Thread.sleep(1000);
+		}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+		driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private void logIn(String login, String password) throws InterruptedException {
+		driver.findElement(By.id("user_login")).clear();
+		driver.findElement(By.id("user_login")).sendKeys(login);
+		driver.findElement(By.id("user_pass")).clear();
+		driver.findElement(By.id("user_pass")).sendKeys(password);
+		driver.findElement(By.id("wp-submit")).click();
+		for (int second = 0;; second++) {
+			if (second >= 60)
+				fail("timeout");
+			try {
+				if (isElementPresent(By.cssSelector("h2.empty-content__title")))
+					break;
+			} catch (Exception e) {
+			}
+			Thread.sleep(1000);
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 }

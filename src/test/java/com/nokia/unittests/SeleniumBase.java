76202a4cc5132bc3e17ac9fd63446c3a26dd13dd
Anna <anna.wojtowicz@alcatel-lucent.com>
2016-06-02 14:31:27 +0200

    WordpressTest version2

---
+++ src/test/java/com/nokia/unittests/SeleniumBase.java
@@ -0,0 +1,105 @@
+package com.nokia.unittests;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumBase {
+
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	public SeleniumBase() {
+		super();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "https://pl.wordpress.com";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	protected void openLogInForm() {
+		driver.get(baseUrl + "/");
+		driver.findElement(By.linkText("Zaloguj się")).click();
+	}
+
+	protected void logOut() throws InterruptedException {
+		driver.findElement(By.cssSelector("img.gravatar")).click();
+	
+		for (int second = 0;; second++) {
+			if (second >= 60)
+				fail("timeout");
+			try {
+				if (isElementPresent(By.xpath("(//button[@type='submit'])[2]")))
+					break;
+			} catch (Exception e) {
+			}
+			Thread.sleep(1000);
+		}
+		driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+	}
+
+	protected void logIn(String login, String password) {
+		driver.findElement(By.id("user_login")).clear();
+		driver.findElement(By.id("user_login")).sendKeys(login);
+		driver.findElement(By.id("user_pass")).clear();
+		driver.findElement(By.id("user_pass")).sendKeys(password);
+		driver.findElement(By.id("wp-submit")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	protected boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+
+}

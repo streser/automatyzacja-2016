b8909f97766c03939ec02025519ce3523c6402f3
lothalev <lothalev@gmail.com>
2016-06-03 12:46:26 +0200

    add @After method

--- src/test/java/com/nokia/integrationtests/TestScrenario.java
+++ src/test/java/com/nokia/integrationtests/TestScrenario.java
@@ -1,17 +1,30 @@
 package com.nokia.integrationtests;
 
+import static org.junit.Assert.fail;
+
+import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestScrenario {
 
-    	protected WebDriver driver;
-    	
-	@Before
-    	public void setUp() {
-    	  driver = new FirefoxDriver();  
-    	}
-    	
-    	
+    protected WebDriver driver;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+
+    @Before
+    public void setUp() {
+	driver = new FirefoxDriver();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+	driver.quit();
+	String verificationErrorString = verificationErrors.toString();
+	if (!"".equals(verificationErrorString)) {
+	    fail(verificationErrorString);
+	}
+    }
+
 }

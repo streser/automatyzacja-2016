0be2750d721b766be90a10d9d25ee11c1f99bfb6
Szlachciak <rafal.szlachciak@alcatel-lucent.com>
2016-06-03 10:43:06 +0200

    postpub_rf2

--- src/test/java/com/nokia/unittests/newPostTest.java
+++ src/test/java/com/nokia/unittests/newPostTest.java
@@ -14,52 +14,47 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.google.common.base.Predicate;
 
 public class newPostTest extends SeleniumBase {
-	
-	private String title; 
-	
+
+	private static final String ACTIVITY_AVATAR_LINK = "//div[2]/div/header/a[3]";
+	private static final String ARTICLE_LINK = "//div/article[1]/a";
+	private String title;
 
 	@Test
 	public void TestPost() throws Exception {
-		title = "Rafal_"+randomName();
-		logIn("szkolenieautomatyzacja", "qw12qw12");
-		insertPost(title);
-		
-		checkPostName();
 
-		
+		logIn("szkolenieautomatyzacja", "qw12qw12");// GIVEN
+
+		title = "Rafal_" + randomName();// WHEN
+		insertPost(title);
+
+		checkPostName(title); // THEN
+
 	}
 
-	private void checkPostName() {
+	private void checkPostName(final String checkedTitle) {
 		driver.get("https://automatyzacjacs.wordpress.com/");
 
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		wait.until(new Predicate<WebDriver>() {
 			public boolean apply(WebDriver input) {
-				return assertThatPostWasAdded(driver, title) != null;
+				return assertThatPostWasAdded(driver, checkedTitle) != null;
 			}
 		});
 	}
 
 	private void insertPost(String title) {
-		click(By.xpath("//div[2]/div/header/a[3]"));
+		click(By.xpath(ACTIVITY_AVATAR_LINK));
 		waitForElement(By.xpath("//div/input"));
 		click(By.className("editor-ground-control__publish-button"));
-		//insertText(title,By.xpath("/html/body/div[2]/div/div[2]/div[1]/div/div/div[1]/div[2]/div[2]/div/input"));
 		insertText(title, By.className("form-text-input"));
-		
-		//click(By.xpath("/html/body/div[2]/div/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/div[3]/div/button[1]"));
 		click(By.className("editor-ground-control__publish-button"));
-		//Thread.sleep(5000);
 		waitForElement(By.linkText("automatyzacjacs"));
 	}
-	
+
 	private WebElement assertThatPostWasAdded(WebDriver driver, String title) {
 		List<WebElement> elemnts = driver.findElements(By.tagName("a"));
 		for (WebElement element : elemnts) {
-			/*
-			 * System.out.println("Old way of debugging;");
-			 * System.out.println(element.getAttribute("href"));
-			 */
+
 			if (element.getAttribute("href").contains(title.toLowerCase())) {
 				return element;
 			}
@@ -67,14 +62,10 @@ public class newPostTest extends SeleniumBase {
 		return null;
 	}
 
-	
 	private void assertThatPostSuccessful() {
-		assertEquals(title, driver.findElement(By.xpath("//div/article[1]/a")).getAttribute("title"));
-		///html/body/div[1]/div/div[2]/main/div/article[1]/a
-		// /html/body/div[1]/div/div[2]/main/div/article[2]/a
-		//html body.home.blog.logged-in.admin-bar.no-customize-support.mp6.customizer-styles-applied.no-featured-posts.highlander-enabled.highlander-light.infinite-scroll div#page.hfeed.site div.site-inner div#content.site-content main#primary.content-area div#posts.posts article#post-247.post-247.post.type-post.status-publish.format-standard.hentry.category-bez-kategorii a.cover-link
+		assertEquals(title, driver.findElement(By.xpath(ARTICLE_LINK)).getAttribute("title"));
 	}
-	
+
 	public void opennew(String path) {
 
 		driver.get(path);
